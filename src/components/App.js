c776ee5d6b54bcd9b30dce7673ac26ba08df8f73
Revanth-Revs <68606788+Revanth-Revs@users.noreply.github.com>
2020-10-25 18:10:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,22 +9,30 @@ class App extends Component {
             posi : 0,
             ballPosition: { left: "0px" }
         };
-        this.renderChoice = this.renderChoice.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
 
+        this.renderChoice = this.renderChoice.bind(this)
+        this.buttonClickHandler = this.buttonClickHandler.bind(this);
+        this.handlekey=this.handlekey.bind(this);
+    };
     //call back function
     buttonClickHandler() {
-   
+        this.setState({renderBall:true});
+        document.addEventListener("keydown",this.handlekey);
    }
     renderChoice() {
 		if(this.state.renderBall){return <div className="ball" style={this.state.ballPosition}></div>}
 		 else   return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
     }
-
+    handlekey(event){
+        console.log(event.key);
+        if(event.key==="ArrowRight")
+        {
+            this.setState({posi:this.state.posi+5,ballPosition:{left:`${this.state.posi+5}px`}});
+        }
+    }
     //bind ArrowRight keydown event
-    componentDidMount() {
-      
+
+    componentDidMount() {     
     }
 
     render() {
@@ -35,6 +43,5 @@ class App extends Component {
         )
     }
 }
-
 
 export default App;
